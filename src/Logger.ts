b99bc4be16206b590702e15a24eb6343604b9d3e
timothy1205 <timothy120500@gmail.com>
2020-06-24 20:19:39 -0600

    Setup logger using winston and an extension

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,1 +1,19 @@
-export default class Logger {}
+import winston from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
+
+let dailyRotateTransport = new DailyRotateFile({
+  filename: "logs/%DATE%.log",
+  datePattern: "YYYY-MM-DD-HH",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "14d",
+});
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.prettyPrint()
+  ),
+  transports: [new winston.transports.Console(), dailyRotateTransport],
+});
