3ea9ab996d114dc059480a6a34e7b043b0f0c587
timothy1205 <timothy120500@gmail.com>
2020-06-25 15:15:49 -0600

    Added username member

--- src/bots/User.ts
+++ src/bots/User.ts
@@ -2,6 +2,7 @@ import { Permission } from "./CommandHandler";
 
 export default abstract class User {
   private permission: Permission;
+  private username: string;
 
   constructor(permission: Permission) {
     this.permission = permission;
@@ -13,5 +14,13 @@ export default abstract class User {
 
   public setPermission(permission: Permission) {
     this.permission = permission;
+  }
+
+  public getUsername() {
+    return this.username;
+  }
+
+  public setUsername(username: string) {
+    this.username = username;
   }
 }
