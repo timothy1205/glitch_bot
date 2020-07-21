5aa70ea9b2cc87c58365c88a969b1132b8a83aad
timothy1205 <timothy120500@gmail.com>
2020-07-21 12:46:24 -0600

    Added placeholder reply for bad arguments on twitch bot

--- src/bots/commands/TwitchCommandHandler.ts
+++ src/bots/commands/TwitchCommandHandler.ts
@@ -1,9 +1,23 @@
 import CommandHandler from "./CommandHandler";
 import IBot from "../IBot";
 import { twitchBotLogger } from "../../Logger";
+import User from "../User";
 
 export default class TwitchCommandHandler extends CommandHandler {
   constructor(bot: IBot) {
     super(bot, twitchBotLogger);
+  }
+
+  protected onBadArguments(
+    user: User,
+    channel: string,
+    alias: string,
+    msg: string
+  ) {
+    // TODO: Create a method (probably on CommandHandler) to generate a command usage string to be used here
+    this.getBot().reply(
+      user,
+      `you specified invalid arguments for "${alias}"...`
+    );
   }
 }
