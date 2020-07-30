af86fabafbcc02e22258508019dfaf2cbb1bb547
timothy1205 <timothy120500@gmail.com>
2020-07-30 15:48:00 -0600

    Better test command

--- src/bots/commands/twitch/test.ts
+++ src/bots/commands/twitch/test.ts
@@ -1,14 +1,19 @@
 import { twitchBot } from "../../../main";
 import Command from "../Command";
-import { HardCallback } from "../Command";
+import { HardCallback, CommandArguments } from "../Command";
 import { Permission } from "../CommandHandler";
 
 twitchBot.getCommandHandler()?.registerCommand(
   new Command<HardCallback>({
     permission: Permission.USER,
     aliases: ["test"],
-    callback: (caller, channel) => {
-      twitchBot.sendChannelMessage("Test message", channel);
+    args: [
+      { arg: CommandArguments.NUMBER, name: "num" },
+      { arg: CommandArguments.STRING, name: "str", optional: true },
+    ],
+    callback: (caller, channel, alias, data) => {
+      if (data)
+        twitchBot.sendChannelMessage(`Test message: ${data[0]}, ${data[1]}`);
     },
   })
 );
