6b3ba987ac0578f6c6537ab4f906ae420861d562
timothy1205 <timothy120500@gmail.com>
2020-08-05 17:59:13 -0600

    Calculate twitch user perms

--- src/bots/TwitchUser.ts
+++ src/bots/TwitchUser.ts
@@ -1,6 +1,20 @@
 import User from "./User";
-import { ChatUserstate } from "tmi.js";
+import { ChatUserstate, Badges } from "tmi.js";
+import { Permission } from "./commands/CommandHandler";
 
+const calculatePerms = (userstate: ChatUserstate) => {
+  if (userstate.badges) {
+    if (userstate["user-id"] === process.env.OWNER_ID) return Permission.OWNER;
+    else if (userstate.badges.broadcaster === "1")
+      return Permission.BROADCASTER;
+    else if (userstate.mod) return Permission.MOD;
+    else if ((userstate.badges as Badges & { vip?: string }).vip === "1")
+      return Permission.VIP;
+    else if (userstate.subscriber) return Permission.SUBSCRIBER;
+  }
+
+  return Permission.USER;
+};
 export default class TwitchUser extends User {
   private userstate: ChatUserstate;
 
@@ -9,5 +23,6 @@ export default class TwitchUser extends User {
     this.userstate = userstate;
 
     if (userstate.username) this.setUsername(userstate.username);
+    this.setPermission(calculatePerms(userstate));
   }
 }
