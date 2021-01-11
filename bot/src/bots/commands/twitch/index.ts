019105435156a09419f291f21bb17512b831bee0
timothy1205 <timothy120500@gmail.com>
2021-01-11 16:21:31 -0700

    Added twitchinfo owner command for convenience

--- bot/src/bots/commands/twitch/index.ts
+++ bot/src/bots/commands/twitch/index.ts
@@ -1,1 +1,2 @@
 export * from "./utils_commands";
+export * from "./twitch_admin_commands";
---
+++ bot/src/bots/commands/twitch/twitch_admin_commands.ts
@@ -0,0 +1,37 @@
+import twitchBot from "../../TwitchBot";
+import { twitchAPI } from "../../../twitch_api";
+import Command, { CommandArguments } from "../Command";
+import { Permission } from "../CommandHandler";
+import twitchCommandHandler from "../TwitchCommandHandler";
+
+twitchCommandHandler.registerCommand(
+  new Command({
+    permission: Permission.OWNER,
+    aliases: ["twitchinfo"],
+    args: [
+      { arg: CommandArguments.STRING, name: "id" },
+      { arg: CommandArguments.STRING, name: "type", optional: true },
+    ],
+    callback: async (caller, _channel, _alias, data, _bot) => {
+      let helixUser;
+      const [id, type] = data as [string, string | undefined];
+
+      try {
+        if (!type || type === "id") {
+          helixUser = await twitchAPI.helix.users.getUserById(id);
+        } else if (type === "name") {
+          helixUser = await twitchAPI.helix.users.getUserByName(id);
+        }
+      } catch (error) {}
+
+      if (!helixUser) {
+        twitchBot.reply(caller, "no user found!");
+      } else {
+        twitchBot.reply(
+          caller,
+          `Name: ${helixUser.name}, DisplayName: ${helixUser.displayName}, ID: ${helixUser.id} ${helixUser.type}, Views: ${helixUser.views}`
+        );
+      }
+    },
+  })
+);
