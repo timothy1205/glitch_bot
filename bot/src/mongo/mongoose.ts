2fb2d0634bdaef5f493f2186ab7b453d2062bb67
timothy1205 <timothy120500@gmail.com>
2021-01-19 22:22:15 -0700

    Autoload static commands from mongodb

--- bot/src/mongo/mongoose.ts
+++ bot/src/mongo/mongoose.ts
@@ -1,5 +1,26 @@
+import { RegisterError } from "./../bots/commands/CommandHandler";
+import { getMongoStaticCommands } from "./staticCommands";
 import mongoose from "mongoose";
 import { mongooseLogger } from "../logging";
+import CommandHandler from "../bots/commands/CommandHandler";
+
+const registerStaticCommands = async () => {
+  mongooseLogger.info("Registering static commands stored in MongoDB!");
+
+  const staticCommands = await getMongoStaticCommands();
+
+  staticCommands.forEach((cmd) => {
+    try {
+      CommandHandler.registerStaticCommand(cmd.aliases, cmd.message);
+    } catch (error) {
+      if (error instanceof RegisterError) {
+        mongooseLogger.warn(
+          `Failed to register static command: ${cmd.aliases} ${cmd.aliases}`
+        );
+      }
+    }
+  });
+};
 
 mongoose.connect("mongodb://mongo:27017/glitch_bot", {
   useNewUrlParser: true,
@@ -12,16 +33,22 @@ connection.on("error", (err) => mongooseLogger.error(err));
 
 connection.on("open", async () => {
   mongooseLogger.info("Successfully connected to MongoDB!");
+  registerStaticCommands();
 });
 
 if (process.env.NODE_ENV === "development") {
-  mongoose.set("debug", function (
-    collectionName: string,
-    methodName: string,
-    ...methodArgs: any[]
-  ) {
-    mongooseLogger.info(
-      `Mongoose: ${collectionName}.${methodName}(${JSON.stringify(methodArgs)})`
-    );
-  });
+  mongoose.set(
+    "debug",
+    function (
+      collectionName: string,
+      methodName: string,
+      ...methodArgs: any[]
+    ) {
+      mongooseLogger.info(
+        `Mongoose: ${collectionName}.${methodName}(${JSON.stringify(
+          methodArgs
+        )})`
+      );
+    }
+  );
 }
