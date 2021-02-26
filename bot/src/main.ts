f3b5f4b0810ed993d9e289237fd405066cfc5238
timothy1205 <timothy120500@gmail.com>
2021-02-26 01:47:45 -0700

    Replace .env with config.json (example included)

--- bot/src/main.ts
+++ bot/src/main.ts
@@ -1,6 +1,11 @@
 // Typescript source mapping
 require("source-map-support").install();
 
+import assert from "assert";
+import config from "../config.json";
+
+validateConfig();
+
 import "./mongo/mongoose";
 import TwitchCommandHandler from "./bots/commands/TwitchCommandHandler";
 import TwitchBot from "./bots/TwitchBot";
@@ -13,3 +18,23 @@ CommandHandler.registerDefaultCommands();
 
 setupPassivePointTimer();
 setupAutoMessageTimerCount();
+
+function validateConfig() {
+  assert(typeof config.mongo_url === "string");
+  assert(typeof config.owner_id === "string");
+  assert(typeof config.twitch_username === "string");
+  assert(typeof config.tmi_oauth_token === "string");
+  assert(typeof config.twitch_client_id === "string");
+  assert(typeof config.twitch_client_secret === "string");
+  assert(typeof config.channels === "object");
+
+  config.channels.forEach((channel) => {
+    assert(typeof channel.username === "string");
+    assert(typeof channel.currency_name === "string");
+    assert(
+      typeof channel.currency_name_plural === "string" ||
+        typeof channel.currency_name_plural === "undefined"
+    );
+    assert(typeof channel.currency_emoji === "string");
+  });
+}
