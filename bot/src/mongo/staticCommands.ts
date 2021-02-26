4df5891a952b921dbd7f12ed601d5a545db20343
timothy1205 <timothy120500@gmail.com>
2021-02-26 16:19:06 -0700

    Restructure staticCommands.ts

--- bot/src/mongo/staticCommands.ts
+++ bot/src/mongo/staticCommands.ts
@@ -1,27 +1,32 @@
 import { removeAutoMessage } from "./../auto_messages";
 import StaticCommandModel from "./models/StaticCommandModel";
 
-export const getMongoStaticCommands = () => {
-  return StaticCommandModel.find({});
+export const getMongoStaticCommands = (channel: string) => {
+  return StaticCommandModel.find({ channel });
 };
 
-export const getMongoStaticCommand = (alias: string) => {
-  return StaticCommandModel.findOne({ aliases: alias });
+export const getMongoStaticCommand = (channel: string, alias: string) => {
+  return StaticCommandModel.findOne({ channel, aliases: alias });
 };
 
 export const setMongoStaticCommand = async (
+  channel: string,
   aliases: string[],
   message: string
 ) => {
   const staticCommand = new StaticCommandModel();
+  staticCommand.channel = channel;
   staticCommand.aliases = aliases;
   staticCommand.message = message;
 
   await staticCommand.save();
 };
 
-export const deleteMongoStaticCommand = async (alias: string) => {
-  const staticCommand = await getMongoStaticCommand(alias);
+export const deleteMongoStaticCommand = async (
+  channel: string,
+  alias: string
+) => {
+  const staticCommand = await getMongoStaticCommand(channel, alias);
 
   if (staticCommand) {
     removeAutoMessage(staticCommand.message);
