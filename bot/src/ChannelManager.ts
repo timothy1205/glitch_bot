9aee88ebba8a7027d3ec09418ffcc9fc0707736e
timothy1205 <timothy120500@gmail.com>
2021-03-08 18:53:42 -0700

    logger, bot, updated some variable names

--- bot/src/ChannelManager.ts
+++ bot/src/ChannelManager.ts
@@ -1,14 +1,36 @@
+import winston from "winston";
+import TwitchBot from "./bots/TwitchBot";
 import Channel, { ChannelConfig } from "./Channel";
+import { createLogger } from "./logging";
 
 class ChannelManager {
-  private channels: { [username: string]: Channel } = {};
+  private _channels: { [username: string]: Channel } = {};
+  private _miscLogger: winston.Logger;
+  private _twitchBot: TwitchBot;
+
+  constructor() {
+    this._miscLogger = createLogger("misc");
+    this._twitchBot = new TwitchBot();
+  }
 
   public createChannel(channelConfig: ChannelConfig) {
-    this.channels[channelConfig.username] = new Channel(channelConfig);
+    this._channels[channelConfig.username] = new Channel(channelConfig);
   }
 
   public getChannel(username: string) {
-    return this.channels[username];
+    return this._channels[username];
+  }
+
+  public get channels() {
+    return this._channels;
+  }
+
+  public get miscLogger() {
+    return this._miscLogger;
+  }
+
+  public get twitchBot() {
+    return this._twitchBot;
   }
 }
 
