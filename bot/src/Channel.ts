d93ab7885e204b12562a0b79a76970da8687cdc0
timothy1205 <timothy120500@gmail.com>
2021-03-08 18:52:58 -0700

    ChannelConfig, updated some variable names

--- bot/src/Channel.ts
+++ bot/src/Channel.ts
@@ -1,6 +1,6 @@
 import winston from "winston";
 import TwitchCommandHandler from "./bots/commands/TwitchCommandHandler";
-import TwitchBot from "./bots/TwitchBot";
+import channelManager from "./ChannelManager";
 import { createLogger } from "./logging";
 
 export interface ChannelConfig {
@@ -11,13 +11,22 @@ export interface ChannelConfig {
 }
 
 export default class Channel {
-  private twitchBotLogger: winston.Logger;
-  private twitchBot: TwitchBot;
+  private _twitchBotLogger: winston.Logger;
+  private _twitchCommandHandler: TwitchCommandHandler;
+  private _twitchConfig: ChannelConfig;
 
   constructor(channelConfig: ChannelConfig) {
-    this.twitchBotLogger = createLogger(`${channelConfig.username}/twitch`);
+    this._twitchBotLogger = createLogger(`${channelConfig.username}/twitch`);
+    this._twitchCommandHandler = new TwitchCommandHandler();
+    this._twitchCommandHandler.setBot(channelManager.twitchBot);
+    this._twitchConfig = channelConfig;
+  }
 
-    const twitchCommandHandler = new TwitchCommandHandler();
-    this.twitchBot = new TwitchBot(twitchCommandHandler, channelConfig);
+  public get twitchBotLogger() {
+    return this._twitchBotLogger;
+  }
+
+  public get twitchConfig() {
+    return this._twitchConfig;
   }
 }
