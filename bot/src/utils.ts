2fe2aac011e1601f0ff6650467611be4e35b7770
timothy1205 <timothy120500@gmail.com>
2021-03-17 15:06:06 -0600

    Rename variable 'func' to 'generateKey'

--- bot/src/utils.ts
+++ bot/src/utils.ts
@@ -68,11 +68,11 @@ export const millisecondsToMinutes = (milliseconds: number) => {
 };
 
 // K = key, V = value
-export const arrayToMap = <K, V>(array: V[], func: (value: V) => K) => {
+export const arrayToMap = <K, V>(array: V[], generateKey: (value: V) => K) => {
   const map: Map<K, V> = new Map();
 
   array.forEach((value) => {
-    map.set(func(value), value);
+    map.set(generateKey(value), value);
   });
 
   return map;
