d02c8580f746d48be3aa860479c6d8d067ac5f0c
timothy1205 <timothy120500@gmail.com>
2021-09-25 00:45:41 -0600

    Added tests for addWatchTime function

--- bot/test/mongo/mongoose.specm.ts
+++ bot/test/mongo/mongoose.specm.ts
@@ -1,6 +1,10 @@
 import assert from "assert";
 import mongoose from "mongoose";
-import { createUser, getUser } from "../../src/mongo/models/UserModel";
+import {
+  addWatchTime,
+  createUser,
+  getUser,
+} from "../../src/mongo/models/UserModel";
 
 describe("Mongoose Tests", () => {
   before((done) => {
@@ -45,6 +49,31 @@ describe("Mongoose Tests", () => {
       it("Get by twitchId/discordId", async () => {
         assert.ok(await getUser({ twitchId: opts.twitchId }));
         assert.ok(await getUser({ discordId: opts.discordId }));
+      });
+    });
+
+    describe("addWatchTime", () => {
+      it("Add time to existing user", async () => {
+        const time = 5;
+        assert.strictEqual(
+          await addWatchTime({ twitchId: opts.twitchId }, time),
+          5
+        );
+        const user = await getUser({ twitchId: opts.twitchId });
+        assert.ok(user);
+        assert.strictEqual(user.minutesWatched, time);
+      });
+
+      it("Add time to non-existing user", async () => {
+        const time = 5;
+        const twitchId = "1234567-2";
+
+        assert.strictEqual(await addWatchTime({ twitchId }, time), time);
+        const user = await getUser({ twitchId });
+        assert.ok(user);
+        assert.strictEqual(user.minutesWatched, time);
+
+        await user.delete();
       });
     });
   });
